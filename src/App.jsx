50a98c630ddc02847ef14afba885efe3bf34e7eb
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-08-23 23:08:25 -0400

    chore: display some todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 import { CreateTodo } from "./CreateTodo.jsx";
 
 function App() {
   // const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    { name: "Buy groceries", complete: false },
+    { name: "Clean the house", complete: true },
+    { name: "Finish project report", complete: false },
+    { name: "Call the bank", complete: true },
+    { name: "Read a book", complete: false },
+  ]);
+
+  let todoItem = todos.map((todo, index) => <li key={index}>{todo.name}</li>);
 
   return (
     <>
       <CreateTodo />
+      <ul>{todoItem}</ul>
     </>
   );
 }
