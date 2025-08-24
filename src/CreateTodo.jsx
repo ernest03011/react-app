cfe2f8339a262137c51de820b073712ecd676253
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-08-23 22:11:35 -0400

    chore: create a component for creating todo items

--- src/CreateTodo.jsx
+++ src/CreateTodo.jsx
@@ -1,13 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
 
 export function CreateTodo() {
   return (
-    <div className="flex w-full max-w-sm items-center gap-2">
-      <Input type="text" placeholder="Create a new TODO" />
-      <Button type="submit" variant="outline">
-        Add TODO
-      </Button>
+    <div className="flex items-center justify-center">
+      <Card className="">
+        <h3>This is a Todo List APP</h3>
+
+        <div>
+          <Input type="text" placeholder="Create a new TODO" />
+          <Button
+            type="submit"
+            variant="outline"
+            className="text-gray-50 hover:text-gray-50"
+          >
+            Add TODO
+          </Button>
+        </div>
+      </Card>
     </div>
   );
 }
