baa31544e4e2c8c7ae0b1e93aaf1fe49de352c8d
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-08-31 20:50:39 -0400

    chore: add styling to the TODO background

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }, [todos]);
 
   return (
-    <>
+    <div className="bg-white px-10 py-10 rounded-3xl flex items-center justify-center flex-col">
       <Heading>
         <h1 className="mb-4 italic font-bold">Your Todo App</h1>
       </Heading>
@@ -26,7 +26,7 @@ function App() {
       {/* I can also add a section or component or part of the Todo that display the completed todos or like a visitbility option to show only 
       completed or not completed  */}
       {/* Add an status Bar component with a list of the items left to be completed, a sorting option with all, active, completed, and a button to delete all completed TODOs */}
-    </>
+    </div>
   );
 }
 
