20f63176f1cf1439e083d1ae8f7ab07b1b7ebe54
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-09-01 21:49:47 -0400

    chore: update styling

--- src/CreateTodo.jsx
+++ src/CreateTodo.jsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card } from "@/components/ui/card";
 import { useState } from "react";
 
 export function CreateTodo({ todos, setTodos }) {
@@ -23,25 +22,26 @@ export function CreateTodo({ todos, setTodos }) {
   }
 
   return (
-    <div className="flex items-center justify-center mb-2">
-      <Card className="">
-        <form onSubmit={handleSubmit}>
-          <Input
-            type="text"
-            name="todo"
-            value={value}
-            placeholder="Create a new TODO"
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <Button
-            type="submit"
-            variant="outline"
-            className="text-gray-50 hover:text-gray-50"
-          >
-            Add TODO
-          </Button>
-        </form>
-      </Card>
+    <div className="mb-4">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-center gap-2 flex-col"
+      >
+        <Input
+          type="text"
+          name="todo"
+          value={value}
+          placeholder="Create a new TODO"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="outline"
+          className="text-gray-50 hover:text-gray-50"
+        >
+          Add TODO
+        </Button>
+      </form>
     </div>
   );
 }
